b8c47f9f146fef2131a25a32c77a387236eaee4a
harrymoon <harrymoon1862@gmail.com>
2025-08-05 10:43:25 +0700

    upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+
+function generateJwt() {
+  // Replace the example values below (remove the brackets).
+  // Store secrets securely based on your team's best practices.
+  // See: https://help.tableau.com/current/online/en-us/connected_apps_direct.htm
+
+  const secret = "[secretvalue]";
+  const secretId = "[connectedAppSecretId]";
+  const clientId = "[connectedAppClientId]";
+  const userId = "[tableau username]";
+  const tokenExpiryInMinutes = 1; // Max of 10 minutes.
+
+  // Remove 'tableau:views:embed_authoring' scope if Authoring is not needed.
+  // Remove 'tableau:insights:embed' scope if Pulse is not needed.
+  const scopes = ["tableau:views:embed", "tableau:views:embed_authoring", "tableau:insights:embed"];
+
+  const userAttributes = {
+    //  User attributes are optional.
+    //  Add entries to this dictionary if desired.
+    //  "[User Attribute Name]": "[User Attribute Value]",
+  };
+
+  const header = {
+    alg: "HS256",
+    typ: "JWT",
+    kid: secretId,
+    iss: clientId,
+  };
+
+  const data = {
+    jti: uuidv4(),
+    aud: "tableau",
+    sub: userId,
+    scp: scopes,
+    exp: Math.floor(Date.now() / 1000) + tokenExpiryInMinutes * 60,
+    ...userAttributes,
+  };
+
+  const token = jwt.sign(data, secret, { header });
+  console.log("Generated JWT:");
+  console.log(token);
+}
+
+generateJwt();
